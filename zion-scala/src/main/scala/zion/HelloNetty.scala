1d66b561351eb3551f858c0b4d37681dddd659f6
Tushar Mathur <tusharmath@gmail.com>
2020-05-05 19:24:51 +0530

    --wip-- [skip ci]

--- zion-scala/src/main/scala/zion/HelloNetty.scala
+++ zion-scala/src/main/scala/zion/HelloNetty.scala
@@ -15,78 +15,30 @@ import io.netty.util.CharsetUtil
 object HelloNetty extends App {
   def helloNetty = "Hello Netty!\n"
 
-  class NettyHandler extends SimpleChannelInboundHandler[AnyRef] {
-    override def channelReadComplete(ctx: ChannelHandlerContext): Unit = {
-      println("FLUSH")
+  class NettyHandler extends SimpleChannelInboundHandler[Nothing] {
+    override def channelReadComplete(ctx: ChannelHandlerContext): Unit =
       ctx.flush
-    }
-
-    def channelRead__1(ctx: ChannelHandlerContext): Unit = {
-      ctx.write(
-        Unpooled.copiedBuffer(HelloResponse.ok(helloNetty), CharsetUtil.UTF_8)
-      )
-    }
-    def channelRead__2(
-        ctx: ChannelHandlerContext
-    ): Unit = {
-
-      val buf = Unpooled.copiedBuffer(
-        helloNetty,
-        CharsetUtil.UTF_8
-      )
-      val response = new DefaultFullHttpResponse(
-        HttpVersion.HTTP_1_1,
-        HttpResponseStatus.OK,
-        buf
-      )
-      response.headers.set(HttpHeaders.Names.CONTENT_LENGTH, buf.readableBytes)
-      response.headers.set(HttpHeaders.Names.CONNECTION, "keep-alive")
-      ctx.write(response)
-    }
-
-    def channelRead__3(
-        ctx: ChannelHandlerContext
-    ): Unit = {
-
-      val buf = Unpooled.copiedBuffer(
-        helloNetty,
-        CharsetUtil.UTF_8
-      )
-      val response = new DefaultFullHttpResponse(
-        HttpVersion.HTTP_1_1,
-        HttpResponseStatus.OK
-      )
-      response.headers.set(HttpHeaders.Names.CONTENT_LENGTH, buf.readableBytes)
-      response.headers.set(HttpHeaders.Names.CONNECTION, "keep-alive")
-
-      println(response.toString)
-      ctx.writeAndFlush(
-        Unpooled.copiedBuffer(List("HTTP/1.1 200 OK").mkString("\r\n\r\n"), CharsetUtil.UTF_8)
-      )
-
-//      println("helloNetty")
-//      ctx.write(
-//        Unpooled
-//          .copiedBuffer("\r\n\r\n" + helloNetty + "\r\n", CharsetUtil.UTF_8)
-//      )
-    }
-
     override def channelRead0(
         ctx: ChannelHandlerContext,
-        msg: scala.AnyRef
+        msg: Nothing
     ): Unit = {
-      channelRead__3(ctx)
+      val buf = Unpooled.copiedBuffer(helloNetty, CharsetUtil.UTF_8)
+      val response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, buf)
+
+      response.headers.set(HttpHeaders.Names.CONTENT_LENGTH, buf.readableBytes)
+      response.headers.set(HttpHeaders.Names.CONNECTION, "keep-alive")
+
+      ctx.write(response)
     }
   }
 
   class NettyServer {
-    val value = new ChannelInitializer[SocketChannel] {
-      def initChannel(socketChannel: SocketChannel) = {
+    val value: ChannelInitializer[SocketChannel] =
+      (socketChannel: SocketChannel) => {
         val pipeline = socketChannel.pipeline
-//        pipeline.addLast(new HttpServerCodec())
+        pipeline.addLast(new HttpServerCodec())
         pipeline.addLast(new NettyHandler())
       }
-    }
 
     def run(): Unit = {
       val eventLoopGroup = new NioEventLoopGroup()
