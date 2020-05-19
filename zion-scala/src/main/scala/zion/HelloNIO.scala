5aeb57102fc9dc6d6dc0d23b05646a3a771fcae1
Tushar Mathur <tusharmath@gmail.com>
2020-05-19 22:51:19 +0530

    refactor(nio): maxing out the performance from HelloNIO.scala

--- zion-scala/src/main/scala/zion/HelloNIO.scala
+++ zion-scala/src/main/scala/zion/HelloNIO.scala
@@ -1,5 +1,6 @@
 package zion
 
+import java.io.IOException
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.{
@@ -9,16 +10,14 @@ import java.nio.channels.{
   SocketChannel
 }
 
-import scala.collection.mutable
-import scala.concurrent.duration._
-
 object HelloNIO {
-  val response: Array[Byte]       = HelloResponse.ok("HelloNIO.scala\n").getBytes()
+  val response: Array[Byte]       = HelloResponse.ok("ok\n").getBytes()
   val writeByteBuffer: ByteBuffer = ByteBuffer.wrap(response)
   val readByteBuffer: ByteBuffer  = ByteBuffer.allocateDirect(1024 * 2)
-  val PORT                        = 8081
-  val selector                    = Selector.open()
-  val address                     = new InetSocketAddress(PORT)
+  val PORT: Int                   = 8081
+  val selector: Selector          = Selector.open()
+  val readWriteSelector: Selector = Selector.open()
+  val address: InetSocketAddress  = new InetSocketAddress(PORT)
 
   def main(args: Array[String]): Unit = {
     val serverSocketChannel = ServerSocketChannel.open()
@@ -33,9 +32,15 @@ object HelloNIO {
       val iterator = keys.iterator()
       while (iterator.hasNext) {
         val key = iterator.next()
-        if (key.isAcceptable) accept(key)
-        else if (key.isReadable) read(key)
-        else if (key.isWritable) write(key)
+        try {
+          if (key.isAcceptable) accept(key)
+          else if (key.isReadable) read(key)
+          else if (key.isWritable) write(key)
+        } catch {
+          case e: IOException =>
+            println(e.getMessage)
+            key.cancel()
+        }
       }
       iterator.remove()
     }
@@ -44,42 +49,30 @@ object HelloNIO {
   def accept(key: SelectionKey): Unit = {
     val serverSocketChannel = key.channel().asInstanceOf[ServerSocketChannel]
     val socketChannel       = serverSocketChannel.accept()
+
     if (socketChannel != null) {
       socketChannel.configureBlocking(false)
+      socketChannel.socket().setKeepAlive(true)
       socketChannel.register(selector, SelectionKey.OP_READ)
     }
+
   }
 
   def read(key: SelectionKey): Unit = {
     val socketChannel = key.channel().asInstanceOf[SocketChannel]
-    tryWithSocket(socketChannel) {
-      val bytes = socketChannel.read(readByteBuffer)
-      readByteBuffer.rewind()
 
-      if (bytes > -1) {
-        key.interestOps(SelectionKey.OP_WRITE)
-      } else {
-        key.interestOps(0)
-      }
+    val count = socketChannel.read(readByteBuffer)
+    if (count == -1) {
+      key.interestOps(0)
+    } else {
+      key.interestOps(SelectionKey.OP_WRITE)
     }
   }
 
   def write(key: SelectionKey): Unit = {
     val socketChannel = key.channel().asInstanceOf[SocketChannel]
-    tryWithSocket(socketChannel) {
-      socketChannel.write(writeByteBuffer.duplicate())
-
-      socketChannel.register(selector, SelectionKey.OP_READ)
-    }
-  }
-
-  def tryWithSocket(socketChannel: SocketChannel)(task: => Unit) = {
-    try {
-      task
-    } catch {
-      case e: Throwable =>
-        e.printStackTrace()
-        socketChannel.close()
-    }
+    socketChannel.write(writeByteBuffer)
+    writeByteBuffer.rewind()
+    key.interestOps(SelectionKey.OP_READ)
   }
 }
