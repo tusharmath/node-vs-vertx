0983dffc4887f1823fd5dfc4e83ce667f8ff7da8
Tushar Mathur <tusharmath@gmail.com>
2020-05-20 11:44:17 +0530

    refactor(NIO): fastest async server

--- zion-scala/src/main/scala/zion/HelloAsyncNIO.scala
+++ zion-scala/src/main/scala/zion/HelloAsyncNIO.scala
@@ -1,6 +1,7 @@
 package zion
 
-import java.net.{InetSocketAddress, SocketOption, StandardSocketOptions}
+import java.io.IOException
+import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.{
   AsynchronousChannelGroup,
@@ -10,59 +11,70 @@ import java.nio.channels.{
 }
 import java.util.concurrent.{Executors, TimeUnit}
 
-object HelloAsyncNIO
-    extends CompletionHandler[AsynchronousSocketChannel, Unit] {
+object HelloAsyncNIO {
+  private val ENABLE_LOGGING = false
   private val group =
-    AsynchronousChannelGroup.withThreadPool(Executors.newSingleThreadExecutor())
+    AsynchronousChannelGroup.withThreadPool(Executors.newFixedThreadPool(24))
 
   private val serverSocketChannel = AsynchronousServerSocketChannel.open(group)
   private val PORT                = 8081
-  private val response            = HelloResponse.ok("HelloNIO.scala\n").getBytes()
+  private val response            = HelloResponse.ok("HelloAsyncNIO.scala\n").getBytes()
   private val writeByteBuffer     = ByteBuffer.wrap(response)
   private val readByteBuffer      = ByteBuffer.allocateDirect(1024 * 2)
 
-  val writeHandler = new CompletionHandler[Integer, AsynchronousSocketChannel] {
-    override def completed(
-        result: Integer,
-        attachment: AsynchronousSocketChannel
-    ): Unit = {
-      attachment.close()
-    }
-    override def failed(
-        exc: Throwable,
-        attachment: AsynchronousSocketChannel
-    ): Unit = {
-      attachment.close()
+  case class Handle[V, A](cb: (V, A) => Unit) extends CompletionHandler[V, A] {
+    override def completed(result: V, attachment: A): Unit =
+      cb(result, attachment)
+    override def failed(cause: Throwable, attachment: A): Unit = {
+      cause match {
+        case e: IOException => log(e.getMessage)
+        case _              => cause.printStackTrace()
+      }
     }
   }
 
-  override def completed(
-      socketChannel: AsynchronousSocketChannel,
-      attachment: Unit
-  ): Unit = {
-    serverSocketChannel.accept((), this)
-
-    socketChannel.setOption(
-      StandardSocketOptions.SO_KEEPALIVE.asInstanceOf[SocketOption[Any]],
-      true
-    )
-
-    socketChannel.write(
-      writeByteBuffer.duplicate(),
-      socketChannel,
-      writeHandler
-    )
+  def log(input: Any*): Unit = {
+    if (ENABLE_LOGGING) println(input.map(_.toString).mkString(", "))
   }
 
-  override def failed(exc: Throwable, attachment: Unit): Unit = {
-    exc.printStackTrace()
-  }
+  private val accept
+      : Handle[AsynchronousSocketChannel, AsynchronousServerSocketChannel] =
+    Handle[AsynchronousSocketChannel, AsynchronousServerSocketChannel](
+      (socket, server) => {
+        log("\nACCEPT")
+        server.accept(serverSocketChannel, accept)
+        readFromSharedBuffer(socket)
+      }
+    )
+
+  private def readFromSharedBuffer(socket: AsynchronousSocketChannel): Unit =
+    socket.read(readByteBuffer.duplicate(), socket, read)
+
+  private def writeToSharedBuffer(socket: AsynchronousSocketChannel): Unit =
+    socket.write(writeByteBuffer.duplicate(), socket, write)
+
+  private val read: Handle[Integer, AsynchronousSocketChannel] =
+    Handle[Integer, AsynchronousSocketChannel]((bytes, socket) => {
+      log("READ", bytes)
+
+      // EOF
+      if (bytes == -1) socket.close()
+
+      // Some data was read and now we can respond back
+      else if (bytes > 0)
+        writeToSharedBuffer(socket)
+
+    })
+  private val write: Handle[Integer, AsynchronousSocketChannel] =
+    Handle[Integer, AsynchronousSocketChannel]((bytes, socket) => {
+      log("WRITE", bytes)
+      readFromSharedBuffer(socket)
+    })
 
   def main(args: Array[String]): Unit = {
     val address = new InetSocketAddress(PORT)
     serverSocketChannel.bind(address)
-
-    serverSocketChannel.accept((), this)
+    serverSocketChannel.accept(serverSocketChannel, accept)
 
     // Making the main thread wait
     group.awaitTermination(Long.MaxValue, TimeUnit.SECONDS)
