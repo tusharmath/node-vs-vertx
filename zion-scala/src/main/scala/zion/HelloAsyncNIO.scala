59adc646efd345fbf72799212f7c1b6a281e38e3
Tushar Mathur <tusharmath@gmail.com>
2020-05-20 13:14:33 +0530

    --wip-- [skip ci]

--- zion-scala/src/main/scala/zion/HelloAsyncNIO.scala
+++ zion-scala/src/main/scala/zion/HelloAsyncNIO.scala
@@ -11,17 +11,43 @@ import java.nio.channels.{
 }
 import java.util.concurrent.{Executors, TimeUnit}
 
+/**
+  * This is potentially as fast as it can get using NIO APIs.
+  */
 object HelloAsyncNIO {
-  private val ENABLE_LOGGING = false
+  // Create a thread pool for the socket channel
+  // It would be better to have probably only one thread for events.
+  // That pool could be shared betwee the SocketServer and in future SocketClients.
   private val group =
     AsynchronousChannelGroup.withThreadPool(Executors.newFixedThreadPool(24))
 
+  // Socket to accept connections
   private val serverSocketChannel = AsynchronousServerSocketChannel.open(group)
-  private val PORT                = 8081
-  private val response            = HelloResponse.ok("HelloAsyncNIO.scala\n").getBytes()
-  private val writeByteBuffer     = ByteBuffer.wrap(response)
-  private val readByteBuffer      = ByteBuffer.allocateDirect(1024 * 2)
 
+  // Port to be used to connect
+  private val PORT = 8081
+
+  // Flag to handle logging
+  private val ENABLE_LOGGING = false
+
+  /**
+    * Contains utilities to manage read/write on the socket channels
+    */
+  object NIOBuffer {
+    private val response        = HelloResponse.ok("HelloAsyncNIO.scala\n").getBytes()
+    private val writeByteBuffer = ByteBuffer.wrap(response)
+    private val readByteBuffer  = ByteBuffer.allocateDirect(1024 * 2) // 2kb
+    def read(
+        socket: AsynchronousSocketChannel
+    )(h: CompletionHandler[Integer, AsynchronousSocketChannel]): Unit =
+      socket.read(readByteBuffer.duplicate(), socket, h)
+    def write(
+        socket: AsynchronousSocketChannel
+    )(h: CompletionHandler[Integer, AsynchronousSocketChannel]): Unit =
+      socket.write(writeByteBuffer.duplicate(), socket, h)
+  }
+
+  // Generic async completion handler
   case class Handle[V, A](cb: (V, A) => Unit) extends CompletionHandler[V, A] {
     override def completed(result: V, attachment: A): Unit =
       cb(result, attachment)
@@ -33,48 +59,50 @@ object HelloAsyncNIO {
     }
   }
 
+  // Logging utility
   def log(input: Any*): Unit = {
     if (ENABLE_LOGGING) println(input.map(_.toString).mkString(", "))
   }
 
-  private val accept
+  private val onAccept
       : Handle[AsynchronousSocketChannel, AsynchronousServerSocketChannel] =
     Handle[AsynchronousSocketChannel, AsynchronousServerSocketChannel](
       (socket, server) => {
         log("\nACCEPT")
-        server.accept(serverSocketChannel, accept)
-        readFromSharedBuffer(socket)
+
+        // Accept new connections immediately
+        server.accept(serverSocketChannel, onAccept)
+
+        // Read from the current socket
+        NIOBuffer.read(socket)(onRead)
       }
     )
 
-  private def readFromSharedBuffer(socket: AsynchronousSocketChannel): Unit =
-    socket.read(readByteBuffer.duplicate(), socket, read)
-
-  private def writeToSharedBuffer(socket: AsynchronousSocketChannel): Unit =
-    socket.write(writeByteBuffer.duplicate(), socket, write)
-
-  private val read: Handle[Integer, AsynchronousSocketChannel] =
+  private val onRead: Handle[Integer, AsynchronousSocketChannel] =
     Handle[Integer, AsynchronousSocketChannel]((bytes, socket) => {
       log("READ", bytes)
 
-      // EOF
+      // EOF, meaning connection can be closed
       if (bytes == -1) socket.close()
 
       // Some data was read and now we can respond back
-      else if (bytes > 0)
-        writeToSharedBuffer(socket)
+      else if (bytes > 0) NIOBuffer.write(socket)(onWrite)
 
     })
-  private val write: Handle[Integer, AsynchronousSocketChannel] =
+
+  private val onWrite: Handle[Integer, AsynchronousSocketChannel] =
     Handle[Integer, AsynchronousSocketChannel]((bytes, socket) => {
       log("WRITE", bytes)
-      readFromSharedBuffer(socket)
+
+      // Read from the socket
+      NIOBuffer.read(socket)(onRead)
     })
 
   def main(args: Array[String]): Unit = {
-    val address = new InetSocketAddress(PORT)
-    serverSocketChannel.bind(address)
-    serverSocketChannel.accept(serverSocketChannel, accept)
+
+    // Setup socket channel
+    serverSocketChannel.bind(new InetSocketAddress(PORT))
+    serverSocketChannel.accept(serverSocketChannel, onAccept)
 
     // Making the main thread wait
     group.awaitTermination(Long.MaxValue, TimeUnit.SECONDS)
