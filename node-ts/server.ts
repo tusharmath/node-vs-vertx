887c4eb352c5495411a048a3ee5bb3d3a4474bde
test <amit.singh@dream11.com>
2021-02-20 14:27:55 +0530

    node(refactor): remove the code for creating cluster

--- node-ts/server.ts
+++ node-ts/server.ts
@@ -3,21 +3,12 @@ import * as cluster from 'cluster'
 import * as os from 'os'
 
 const port = 8000
-const numCPUs = os.cpus().length
 
-if (cluster.isMaster) {
-  for (let i = 0; i < numCPUs; i++) {
-    const worker = cluster.fork()
-    worker.on('exit', (code, signal) => {
-      console.log(`worker ${worker.process.pid} died`)
-    })
-  }
-} else {
   http
     .createServer((req, res) => {
       res.end('hello world\n')
     })
     .listen(port)
 
-  console.log(`Worker ${process.pid} started ${port}`)
-}
+  console.log(`server started ${port}`)
+
